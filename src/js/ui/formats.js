4535fa2b1164936254a7e9ed1674050f6f29ef70
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-02-11 09:31:39 +1300

    [New] formats: attr for attributes output

--- src/js/ui/formats.js
+++ src/js/ui/formats.js
@@ -1,11 +1,12 @@
-import { type } from "./utils.js";
+import { bool as _bool, type } from "./utils.js";
 
 // --
 // ## Formats
-export const bool = (_) => (_ ? true : false);
+export const bool = _bool;
 export const text = (_) => `${_}`;
+export const attr = (_) => (bool(_) ? text(_) : "");
 export const not = (_) => !bool(_);
 export const idem = (_) => _;
 export const format = (value, format) => (Formats[format] || idem)(value);
-export const Formats = { bool, text, not, idem, type };
+export const Formats = { bool, text, not, idem, type, attr };
 // EOF
