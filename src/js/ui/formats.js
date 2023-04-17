514e7561e467309981c2f1b11b05b4c73e41cc18
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-04-17 20:59:31 +1200

    [New] formats: debug format to make debugging easier

--- src/js/ui/formats.js
+++ src/js/ui/formats.js
@@ -8,5 +8,9 @@ export const attr = (_) => (bool(_) ? text(_) : "");
 export const not = (_) => !bool(_);
 export const idem = (_) => _;
 export const format = (value, format) => (Formats[format] || idem)(value);
-export const Formats = { bool, text, not, idem, type, attr, len };
+export const debug = (value) => {
+  console.log("[uijs.debug] Slot value:", { value });
+  return value;
+};
+export const Formats = { bool, text, not, idem, type, attr, len, debug };
 // EOF
