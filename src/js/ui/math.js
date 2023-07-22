640efd7068cb30c385331c18e80200f7cc3fa705
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-07-22 12:03:52 +1200

    [New] math: vector functions

--- src/js/ui/math.js
+++ src/js/ui/math.js
@@ -5,4 +5,30 @@ export const lerp = (a, b, k) => {
 };
 export const prel = (v, a, b) => (v - a) / (b - a);
 
+export const point = (x = 0, y = 0) => [x, y, 0, 0];
+export const rect = (x = 0, y = 0, w = 1, h = 1) => [x, y, w, h];
+export const vmul = ([ax, ay, aw, ah], [bx, by, bw, bh]) => [
+  ax * bx,
+  ay * by,
+  aw * bw,
+  ah * bh,
+];
+export const vdiv = ([ax, ay, aw, ah], [bx, by, bw, bh]) => [
+  ax / bx,
+  ay / by,
+  aw / bw,
+  ah / bh,
+];
+export const vadd = ([ax, ay, aw, ah], [bx, by, bw, bh]) => [
+  ax + bx,
+  ay + by,
+  aw + bw,
+  ah + bh,
+];
+export const vsub = ([ax, ay, aw, ah], [bx, by, bw, bh]) => [
+  ax - bx,
+  ay - by,
+  aw - bw,
+  ah - bh,
+];
 // EOF
