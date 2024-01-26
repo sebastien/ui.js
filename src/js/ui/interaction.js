f002d15135842074ba966d3618d0c42d03f5ed6f
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-01-27 10:28:54 +1300

    [New] interaction: keyboard controls

--- src/js/ui/interaction.js
+++ src/js/ui/interaction.js
@@ -1,4 +1,7 @@
 import { list } from "./utils/collections.js";
+import { def } from "./utils/func.js";
+
+// NOTE: Should probably be split up in submodules
 
 export const bind = (node, handlers) =>
 	(handlers &&
@@ -52,5 +55,77 @@ export const autoresize = (event) => {
 	// That's if the box-sizing is border-box.
 	node.style.height = border + node.scrollHeight + "px"; // Adjust hei
 };
+
+// Translated from FF-Kit's Stdlibs
+export class Keyboard {
+	static Down = "keydown";
+	static Up = "keyup";
+	static Press = "press";
+
+	static Codes = {
+		SPACE: 32,
+		TAB: 9,
+		ENTER: 13,
+		COMMA: 188,
+		COLON: 186,
+		BACKSPACE: 8,
+		INSERT: 45,
+		DELETE: 46,
+		ESC: 27,
+		UP: 38,
+		DOWN: 40,
+		LEFT: 37,
+		RIGHT: 39,
+		PAGEUP: 33,
+		PAGEDOWN: 34,
+		HOME: 36,
+		END: 35,
+		SHIFT: 16,
+		ALT: 18,
+		CTRL: 17,
+		META_L: 91,
+		META_R: 92,
+	};
+
+	// --
+	// Returns the name of the key pressed. This takes into account
+	// keyboard layout and modifiers.
+	static Key(event) {
+		return event ? def(event.key, event.keyIdentifier, null) : null;
+	}
+
+	// --
+	// Returns the code of the key. This does not take into account
+	// keyboard layout, it just returns the number of the key.
+	static Code(event) {
+		// SEE: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
+		//SEE: http://caniuse.com/#feat=keyboardevent-code
+		return event ? def(event.keyCode, null) : null;
+	}
+
+	// --
+	// Returns the character that would be typed by the event. This relies
+	// on `Key` to work.
+	//
+	// Note that the often recommended `String.fromCharCode(event.keyCode)`
+	// does not work as you would expect. The problem is that you can't use `keyCode` because it can
+	// translate into different characters based on modifiers and
+	// keyboard layout. For instance pressing 8 gives keyCode=56,
+	// but pressing shift+8 gives also keyCode=56, while the
+	// key is "*" on FR_CA keyboard.
+	static Char(event) {
+		const k = Keyboard.Key(event);
+		return !k ? null : k.length === 1 ? k : k === "Enter" ? "\n" : null;
+	}
+
+	static IsControl(event) {
+		const k = Keyboard.key(event);
+		return k && k.length > 1;
+	}
+
+	static HasModifier(event) {
+		return event && (event.altKey || event.ctrlKey);
+	}
+}
 
 // EOF
