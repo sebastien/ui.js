15742413db324f99443b8874cfda5aa79326a818
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-01-27 10:27:45 +1300

    [New] examples: observability

---
+++ research/observability-data.py
@@ -0,0 +1,9 @@
+"""
+Give an log entry format like so:
+- level: one of "debug", "log", "warning", "error"
+- message: a description of the error message, for instance "Invalid input"
+- structured data for the context, for instance `{Value:"asdad12312dasd", Application:"Web API",Environment:"Production"}`
+- timestamp: an EPOCH UTC timestamp in seconds (float)
+
+Could you generate 20 sample entries or more in JSON format that would be representative of a log journal of a web application backend? Try to have an example for each in JSON format, and make the UTC timestamp start in 2024
+"""
